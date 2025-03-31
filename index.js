5579cc1f2dbf2e0cfe3f28cf3f4632f5a0ce1b92
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-31 11:12:26 +0100

    - Removed unnecessary break lines. - Corrected the ItemNumber elements created. - Improved wrapping and width for the #container.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+// 16x16 grid of square divs
 function gridMaker(rowItemNumber) {
     const container = document.querySelector("#container");
-    const itemTotal = rowItemNumber * rowItemNumber + rowItemNumber;
-    const mod = rowItemNumber + 1;
+    container.innerHTML = "";
 
-    for (var i = 1; i <= itemTotal; i++) {
+    // 25px box + 1px border
+    container.style.width = `${rowItemNumber * 26}px`;
+    container.style.display = "flex";
+    container.style.flexWrap = "wrap";
+
+  for (let i = 0; i < rowItemNumber * rowItemNumber; i++) {
     const div = document.createElement("div");
-
-    if (i % mod === 0) {
-        div.style.cssText = "border: 0; height: 0; width: 100%";
-    } else {
-        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-    }
-
+    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
     container.appendChild(div);
     }
 }
