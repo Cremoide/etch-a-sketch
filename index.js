febc4a09104943b561cc8b405e674a06fce45d04
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-31 12:48:58 +0100

    - New class added to the tiles generated on the gridMaker. - Hover effect is workin when mouse goes over the tiles. (Div’s background color change using JS.)

--- index.js
+++ index.js
@@ -11,8 +11,44 @@ function gridMaker(rowItemNumber) {
   for (let i = 0; i < rowItemNumber * rowItemNumber; i++) {
     const div = document.createElement("div");
     div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    div.classList.add('cell')
     container.appendChild(div);
     }
 }
 
 gridMaker(16);
+
+// Hover effect on the tiles when mouse goes over
+const cell = document.querySelectorAll(".cell");
+
+cell.forEach(cell => {
+    cell.addEventListener("mouseenter", () => {
+        console.log("Cell detected!");
+        cell.style.background = "black";
+    });
+});
+
+// for (let i = 0; i < cell.length; i++) {
+//     cell[i].style.backgroundColor = "black";
+// }
+
+// cellNodeList.addEventListener("mouseenter", cellPaint);
+
+
+// for (let i = 0; i < cell.length; i++) {
+//     cell[i].style.backgroundColor = "black";
+// }
+
+// cellNodeList.addEventListener("mouseenter", cellPaint);
+
+// element.addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+//   });
+
+// btn.onclick = () => {
+//     const cellNodeList = document.querySelectorAll(".cell");
+
+//     for (let i = 0; i < cell.length; i++) {
+//         cell[i].style.backgroundColor = "black";
+//     }
+// };
