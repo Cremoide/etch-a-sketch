065f0f820824061608e26ad9c74c26fe2c2a6794
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-04-02 12:44:59 +0100

    Assignment completed successfully. /////////////////////////////////////////////////////////////////////////// - New feature: Button for random color mode! - Small fixes on the layout!

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 // Popup button for the new grid
-const btn = document.querySelector("#btn");
+const canvasBtn = document.querySelector("#canvasBtn");
 
-btn.addEventListener("click", (e) => {
+canvasBtn.addEventListener("click", (e) => {
     let userInput = prompt("How big you want your grid?", "1-100");
 
     e = Number(userInput);
 
     if (Number.isNaN(e)) {
-        alert("Sadly, I can only take numbers from you.");
-        getInput();
+    alert("Sadly I can only take numbers from you.");
+    getInput();
     }
     if (userInput <= 0) {
-        alert("C'mon you know you can do better than that!");
-        console.log(e);
-        return e;
+    alert("C'mon, you know you can do better than that!");
+    console.log(e);
+    return e;
     }
     if (userInput > 100) {
-        console.log(e);
-        alert("Beep-bop! Toaster can't go over 100x100 grid size.");
-        console.log(e);
-        return e;
+    console.log(e);
+    alert("Beep-bop! Toaster can't go over 100x100 grid size.");
+    console.log(e);
+    return e;
     } else {
-        gridMaker(e);
+    gridMaker(e);
+    }
+});
+
+// Button for activating random color
+const colorBtn = document.querySelector("#colorBtn");
+
+colorBtn.addEventListener("click", (e) => {
+    if (e.target.textContent === `Random colors?`) {
+        rainbowPaint();
+        colorBtn.textContent = `Back to black?`;
+    } else if (e.target.textContent === `Back to black?`) {
+        colorBtn.textContent = `Random colors?`;
+        blackPaint();
     }
 });
 
@@ -33,30 +46,65 @@ function gridMaker(rowItemNumber) {
     const containerWidth = container.clientWidth;
     const containerHeight = container.clientHeight;
 
-    const cellWidth = containerWidth / rowItemNumber
-    const cellHeight = containerHeight / rowItemNumber
+    const cellWidth = containerWidth / rowItemNumber;
+    const cellHeight = containerHeight / rowItemNumber;
 
     // Tile generation
     for (let i = 0; i < rowItemNumber * rowItemNumber; i++) {
     const cell = document.createElement("div");
 
-    console.log(`Tile Width: ${Math.floor(containerWidth / rowItemNumber)}px`);
-    console.log(`Tile Height: ${Math.floor(containerHeight / rowItemNumber)}px`);
-
     cell.style.cssText = `height: ${cellHeight}; width: ${cellHeight}px`;
-    cell.classList.add('cell')
+    cell.classList.add("cell");
     container.appendChild(cell);
     }
 
-    // Hover effect on the tiles when mouse goes over
+    blackPaint();
+}
+
+gridMaker(16);
+
+
+// PAINTING MODES
+
+// Hover effect on the tiles when mouse is over
+function blackPaint() {
     let cell = document.querySelectorAll(".cell");
 
     cell.forEach((cell) => {
         cell.addEventListener("mouseenter", () => {
-            console.log("Cell detected!");
             cell.style.background = "black";
         });
     });
-};
+}
 
-gridMaker(16);
+// Hover RAINBOW on tiles when mouse is over
+function rainbowPaint() {
+    let cell = document.querySelectorAll(".cell");
+
+    cell.forEach((cell) => {
+        cell.addEventListener("mouseenter", () => {
+
+            function getRandomColor() {
+                const r = Math.floor(Math.random() * 256);
+                const g = Math.floor(Math.random() * 256);
+                const b = Math.floor(Math.random() * 256);
+                return `rgb(${r}, ${g}, ${b})`;
+            }
+            cell.style.background = getRandomColor();
+        });
+    });
+}
+
+// Hover PICTOCHAT on tiles when mouse is over (WorkInProgress)
+function pictoChatPaint() {
+    let cell = document.querySelectorAll(".cell");
+
+    cell.forEach((cell) => {
+        cell.addEventListener("mouseenter", () => {
+                let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+                for(var i = 0; i < colors.length; i ++) {
+                    cell.style.background = colors[i];
+                }
+        });
+    });
+}
